7e3cf7095571185993094d411303feae74ab667d
Victor Tseng <palatis@gmail.com>
2017-02-06 02:05:20 +0800

    remove unnecesary deps from VB projects

--- extras/VisualBasic/ArduinoController/ControllerForm.vb
+++ extras/VisualBasic/ArduinoController/ControllerForm.vb
@@ -1,11 +1,5 @@
 Imports System
-Imports System.Collections.Generic
-Imports System.ComponentModel
-Imports System.Data
-Imports System.Drawing
 Imports System.Globalization
-Imports System.Linq
-Imports System.Text
 Imports System.Windows.Forms
 Imports Microsoft.VisualBasic
 
