c2d7d1f2d3d33c29105682fd99a222a18dd79328
Victor Tseng <palatis@gmail.com>
2017-02-06 01:39:08 +0800

    add a helper project to handle InvokeRequired stuff

---
+++ extras/CSharp/CommandMessenger.Utils/InvokeUtils.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace CommandMessenger.Utils
+{
+    public static class InvokeUtils
+    {
+        public static void InvokeIfRequired(this ISynchronizeInvoke invokableObject, MethodInvoker action)
+        {
+            if (invokableObject.InvokeRequired)
+                invokableObject.Invoke(action, new object[0]);
+            else
+                action();
+        }
+    }
+}
